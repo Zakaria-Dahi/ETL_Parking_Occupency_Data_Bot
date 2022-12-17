f8630d64c627b437447ae8e05ef478454f99c820
zak <dahi_zakaria@hotmail.fr>
2022-12-17 18:11:26 +0100

    Commit done on 17-12-2022

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # ETL_Parking_Occupency_Data_Bot
 
-**Programmer:** Zakaria Abdelmoiz DAHI, University of Malaga, Spain.
+**Programmer:** Zakaria Abdelmoiz DAHI, University of Malaga, Spain. :shipit:
 
 **About:** This repository contains the ETL pipline for parking availability data in the city of Malaga Spain starting from October 2022:
 
-	* Extraction: The code is extracting the data every 15, 30 and 60 minutes of 14 public parkings using an API scrapping bot. 
-	* Transformation: It does include kidn of transformation for treating the data.
-	* Loading: this is done byt performing automatic commits to the github repository every 24 hours. 
+	- Extraction: The code is extracting the data every 15, 30 and 60 minutes of 14 public parkings using an API scrapping bot. 
+	- Transformation: It does include kidn of transformation for treating the data.
+	- Loading: this is done byt performing automatic commits to the github repository every 24 hours. 
 	
 **Goal:** The idea is to feed data to a backend algorithm (e.g. a machine learning model) and make the best use of it.  
---
+++ scrap.py
@@ -0,0 +1,53 @@
+import requests
+import time
+import urllib
+import json
+from datetime import datetime
+
+
+#-----------------------------------------------------------------------------
+# Start of scrapping: Start 22-10-2022
+# Data source:
+# https://www.gti.ssr.upm.es/data/parking-lot-database
+# http://cnrpark.it/
+# https://www.kaggle.com/datasets/mypapit/klccparking?resource=download
+# https://datosabiertos.malaga.eu/dataset/ocupacion-aparcamientos-publicos-municipales/resource/0dcf7abd-26b4-42c8-af19-4992f1ee60c6
+#https://datosabiertos.malaga.eu/dataset/ocupacion-aparcamientos-publicos-municipales
+#-----------------------------------------------------------------------------
+
+import os
+
+# Initialising Git
+os.system("git init")
+os.system("git remote set-url origin git@github.com:Zakaria-Dahi/ETL_Parking_Occupency_Data_Bot.git")
+
+
+for m in range(12): # recover the data during the 12 months of the year
+    for l in range(4): # recover the 4 weeks of the month
+        for k in range (7): # recover the data during 7 days
+            for j in range(24): # recover data during 24 hours.
+                for i in range(4): # recover data every 1/4 hour.
+                    url = 'https://datosabiertos.malaga.eu/recursos/aparcamientos/ocupappublicosmun/ocupappublicosmun.csv'
+                    r = requests.get(url,allow_redirects=True)
+                    name = "results/"+str(datetime.now()) +'.csv';
+                    open(name,'wb').write(r.content)
+                    time.sleep(900)
+                # Uploading the results to Github every one hour
+                os.system("git add .")
+                commit_name = "git commit -m \"Commit done on:" + str(datetime.now()) + "\""
+                print(commit_name)
+                os.system(commit_name)
+                os.system("git push origin main")
+
+
+# This is just an alternative method for scrapping using the open API.
+"""
+for i in range(1):
+    url = 'https://datosabiertos.malaga.eu/api/3/action/datastore_search?resource_id=0dcf7abd-26b4-42c8-af19-4992f1ee60c6&limit=100'
+    fileobj = urllib.request.urlopen(url)
+    json_object = json.dumps(fileobj.read().decode())
+    name = 'data('+ str(i) +').json';
+    with open(name,"w") as outfile:
+        outfile.write(json_object)
+"""
+
